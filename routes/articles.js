31d969cac56a0e680044967ee48d6c993bd1b10d
richardljohn <richardljohn@gmail.com>
2022-11-14 17:04:50 -0500

    Formatting

--- routes/articles.js
+++ routes/articles.js
@@ -16,16 +16,17 @@ router.post('/', async(req, res) => {
     let article = new Article({
         title: req.body.title,
         description: req.body.description,
-        markdown: req.body.markdown
+        markdown: req.body.markdown,
     })
     
     try { 
         article = await article.save()
-        
         res.redirect(`/articles/${article.id}`)
-    } catch (e) { 
+    } 
+    catch (e) { 
         console.log(e)
         res.render('articles/new', {article: article})
+
     }
 })
 
